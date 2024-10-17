710d681bebeb502b5883f78934f12e6a8ef09bbe
NOCanoa <158103554+NOCanoa@users.noreply.github.com>
2024-10-18 00:09:27 +0100

    Atualizar o next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,17 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = (phase, { defaultConfig }) => {
-  const defaultConfigWWW = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "raw.githubusercontent.com",
-        },
-        {
-          protocol: "https",
-          hostname: "cdn.jsdelivr.net",
-          port: "",
-          pathname: "/gh/zen-browser/**",
-        },
-      ],
-      domains: ["localhost", "cdn.jsdelivr.net", "raw.githubusercontent.com"], // Allow images from jsDelivr
-    },
-    experimental: {
-      serverActions: {
-        // edit: updated to new key. Was previously `allowedForwardedHosts`
-        allowedOrigins: ["localhost:3000", "get-zen.vercel.app"],
-      },
-    },
-    compiler: {
-      styledComponents: true,
-    },
-  };
-};
-
-module.exports = {
-  ...nextConfig,
-  output: "static",
-  distDir: "out",
-  
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
