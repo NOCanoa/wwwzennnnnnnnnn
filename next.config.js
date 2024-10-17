9ec6aa9fbd2652e9a8b020ea21919af948a8d442
NOCanoa <158103554+NOCanoa@users.noreply.github.com>
2024-10-17 20:38:36 +0100

    ...

--- next.config.js
+++ next.config.js
@@ -2,30 +2,49 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = (phase, { defaultConfig }) => {
-	const defaultConfigWWW = {
-		images: {
-			remotePatterns: [
-				{
-					protocol: "https",
-					hostname: "raw.githubusercontent.com",
-				},
-				{
-					protocol: "https",
-					hostname: "cdn.jsdelivr.net",
-					port: "",
-					pathname: "/gh/zen-browser/**",
-				},
-			],
-			domains: ["localhost", "cdn.jsdelivr.net", "raw.githubusercontent.com"], // Allow images from jsDelivr
-		},
+  const defaultConfigWWW = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "raw.githubusercontent.com",
+        },
+        {
+          protocol: "https",
+          hostname: "cdn.jsdelivr.net",
+          port: "",
+          pathname: "/gh/zen-browser/**",
+        },
+      ],
+      domains: ["localhost", "cdn.jsdelivr.net", "raw.githubusercontent.com"], // Allow images from jsDelivr
+    },
+    experimental: {
+      serverActions: {
+        // edit: updated to new key. Was previously `allowedForwardedHosts`
+        allowedOrigins: ["localhost:3000", "get-zen.vercel.app"],
+      },
+    },
+    compiler: {
+      styledComponents: true,
+    },
+  };
 
-		compiler: {
-			styledComponents: true,
-		},
-	};
-	return {
-		...defaultConfigWWW,
-		// production only config options here
-		output: "export",
-	};
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...defaultConfigWWW,
+      // development only config options here
+    };
+  }
+  return {
+    ...defaultConfigWWW,
+    // production only config options here
+    target: "serverless",
+    exportPathMap: async function () {
+      return {
+        "/": { page: "/" },
+      };
+    },
+  };
 };
+
+module.exports = nextConfig;
