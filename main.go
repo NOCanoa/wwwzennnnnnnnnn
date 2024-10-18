372061f44560e54bae5027839630e13c419e83ec
NOCanoa <158103554+NOCanoa@users.noreply.github.com>
2024-10-18 17:23:00 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -10,48 +11,46 @@ import (
 )
 
 func uploadFilesToBunnyCDN(storageZoneName, region, apiKey string) error {
-	outDir := "./dist"
-	files, err := os.ReadDir(outDir)
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		filePath := filepath.Join(outDir, file.Name())
-		fileBytes, err := os.ReadFile(filePath)
+	distDir := "./dist"
+	return filepath.Walk(distDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-
-		req, err := http.NewRequest("PUT", fmt.Sprintf("https://%s.bunnycdn.com/%s/%s", region, storageZoneName, file.Name()), bytes.NewReader(fileBytes))
-		if err != nil {
-			return err
+		if !info.IsDir() {
+			fileBytes, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			relPath, err := filepath.Rel(distDir, path)
+			if err != nil {
+				return err
+			}
+			req, err := http.NewRequest("PUT", fmt.Sprintf("https://%s.bunnycdn.com/%s/%s", region, storageZoneName, relPath), bytes.NewReader(fileBytes))
+			if err != nil {
+				return err
+			}
+			req.Header.Set("AccessKey", apiKey)
+			req.Header.Set("Content-Type", "application/octet-stream")
+			req.Header.Set("Accept", "application/json")
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+				return fmt.Errorf("upload failed with status code %d", resp.StatusCode)
+			}
+			log.Printf("Uploaded %s to BunnyCDN\n", relPath)
 		}
-
-		req.Header.Set("AccessKey", apiKey)
-		req.Header.Set("Content-Type", "application/octet-stream")
-		req.Header.Set("Accept", "application/json")
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return err
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			return fmt.Errorf("upload failed with status code %d", resp.StatusCode)
-		}
-
-		log.Printf("Uploaded %s to BunnyCDN\n", file.Name())
-	}
-
-	return nil
+		return nil
+	})
 }
 
 func main() {
-	storageZoneName := "your_storage_zone_name"
+	storageZoneName := "where"
 	region := "storage"
-	apiKey := "your_api_key"
+	apiKey := "nokey"
+
 	if len(os.Args) >= 3 {
 		apiKey = os.Args[1]
 	}
