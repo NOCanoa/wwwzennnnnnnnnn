6eb1a0121d9e589cccf2259422c4b652131a37e4
NOCanoa <158103554+NOCanoa@users.noreply.github.com>
2024-12-15 18:05:29 +0000

    trying whit using env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import { defineConfig } from 'astro/config';
+
+const prId = process.env.PR_ID || 'default';  // Use 'default' if PR_ID is not set
+
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    site: 'https://prs-zen-browser-site.b-cdn.net/',
+    site: `https://prs-zen-browser-site.b-cdn.net/${prId}`,
 });
