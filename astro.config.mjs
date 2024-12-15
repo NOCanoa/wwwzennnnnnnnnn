85a48a9f66ee00f2597a829a7f20204495952126
NOCanoa <158103554+NOCanoa@users.noreply.github.com>
2024-12-15 18:14:20 +0000

    why not work

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-const prId = process.env.PR_ID || 'default';  // Use 'default' if PR_ID is not set
+/* const prId = process.env.PR_ID || 'default'; */  // Use 'default' if PR_ID is not set
 
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    site: `https://prs-zen-browser-site.b-cdn.net/${prId}`,
+    site: `https://prs-zen-browser-site.b-cdn.net/1`,
 });
